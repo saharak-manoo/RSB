26399bf57c43fda65822d05dd5baca651aab2899
Saharak Manoo <saharakmanoo@gmail.com>
2022-01-27 20:42:22 +0700

    [Fixed] gem

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    @annual_sales = Order.sum(:price)
+    @sales_past_3_month = Order.where(sold_at: 3.month.ago.to_date..Time.now.to_date).sum(:price)
+    @sales_1_month = Order.where(sold_at: Time.now.beginning_of_month.to_date..Time.now.to_date).sum(:price)
   end
 end
